ca54319741036cbbefe643eb96c7bb29d4781c72
Pat Myron <pmyron09@gmail.com>
2015-05-19 20:24:48 -0400

    .

--- subreddit-leaders/script.js
+++ subreddit-leaders/script.js
@@ -38,13 +38,12 @@ function addRows () {	// ENTRY POINT     int main() lol
 			}
 			map[user] += score;
 		} 
-		
+			
 		/************************************
 		IMPLEMENT A SORT BY VALUE
 		THAT PRESERVES THE SCORE (VALUE)
 		**********************************/
 		var keysSorted = getSortedKeys(map);
-		console.log(keysSorted);
 
 		for (var i = 0; i < keysSorted.length; i++) {
 			var key = keysSorted[i];
@@ -54,27 +53,5 @@ function addRows () {	// ENTRY POINT     int main() lol
 }
 function getSortedKeys(obj) {
     var keys = []; for(var key in obj) keys.push(key);
-    return keys.sort(function(a,b){return obj[a]-obj[b]});
+    return keys.sort(function(a,b){return obj[b]-obj[a]});
 }
-/*
-function addRow (cellOne, cellTwo, cellThree, cellFour) {
-	// Find a <table> element with id="myTable":
-	var table = document.getElementById("myTable");
-
-	// Create an empty <tr> element and add it to the 1st position of the table:
-	var row = table.insertRow(-1);
-
-	// Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
-	var cell1 = row.insertCell(0);
-	var cell2 = row.insertCell(1);
-	var cell3 = row.insertCell(2);
-	var cell4 = row.insertCell(3);
-
-
-	// Add some text to the new cells:
-	cell1.innerHTML = cellOne;
-	cell2.innerHTML = cellTwo;
-	cell3.innerHTML = cellThree;
-	cell4.innerHTML = cellFour;
-}
-*/
