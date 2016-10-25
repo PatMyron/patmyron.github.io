0a911d0f16c61afd4bfdf665adb8b2c7bf30aa35
Pat Myron <pmyron09@gmail.com>
2016-10-24 20:20:12 -0400

    resume

--- doc/script.py
+++ doc/script.py
@@ -1,13 +1,17 @@
 import tinify
+import requests
+from requests.auth import HTTPBasicAuth
+
+location = "../resume.png"
+
 tinify.key = "ByxBoyxbDYigjBW61tWieDRIgblgDIE2"
-source = tinify.from_file("resume.png")
+source = tinify.from_file(location)
 source = source.resize(
     method="fit",
     width=850,
     height=1100
 )
-source.to_file("resume.png")
-source = tinify.from_file("resume.png")
-source.to_file("resume.png")
-source = tinify.from_file("resume.png")
-source.to_file("resume.png")
+for i in range(0,5):
+	source.to_file(location)
+
+zamzar_key = '1ba5713e2226e28f92e8e7a9ebb1e41673c23ba2'
